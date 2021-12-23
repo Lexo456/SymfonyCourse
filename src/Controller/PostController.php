09ac6c60147233eb16a27d6544488786edc9bc28
Pierre Delville <delvillemail@gmail.com>
2021-12-23 10:23:33 +0100

    Post controller Remove

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\ManagerRegistry as DoctrineManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,19 +16,55 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
+
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts
         ]);
     }
     #[Route('/create', name: 'create')]
-    public function create(Request $request)
+    public function create(Request $request, ManagerRegistry $manager)
     {
         $post = new Post();
 
         $post->setTitle('This is going to be a title');
 
+        $em = $manager->getManager();
+
+        $em->persist($post);
+
+        $em->flush();
+
         return new Response('Post was created');
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * @param Post $post
+     */
+    public function show($id, PostRepository $postRepository): Response
+    {
+        $post = $postRepository->find($id);
+
+        return $this->render('post/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+    /**
+     * @Route("delete/{id}", name="delete")
+     */
+    public function remove($id, ManagerRegistry $manager, PostRepository $postRepository): Response
+    {
+        $em = $manager->getManager();
+
+        $post = $postRepository->find($id);
+
+        $em->remove($post);
+
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('post.index'));
     }
 }
