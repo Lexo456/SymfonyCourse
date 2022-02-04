050842d07192a8894af0158b8b65cf466c7d2907
Pierre Delville <49648387+Lexo456@users.noreply.github.com>
2022-02-04 13:06:15 +0100

    refactor post

--- tests/TestHomePage.php
+++ tests/TestHomePage.php
@@ -1,13 +1,17 @@
 <?php
- namespace App\Tests;
 
- use Symfony\Bundle\FrameworkBundle\Test\WebTestCase
+namespace App\Tests;
 
- class TestHomePage extends WebTestCase{
-     public function testHomePage():void
-     {
-         $client = static::createClient();
-         $client->request('GET', '/');
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-     }
- }
+class TestHomePage extends WebTestCase
+{
+    public function testHomePage(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $response = $client->getResponse();
+        self::assertResponseStatusCodeSame(200);
+        $responseData = json_decode($response->getContent(), true);
+    }
+}
